528ddacfb7a92a9d28a43446de75a660e715960a
Mike Santos <mike.santos@contraprova.com.br>
2021-04-05 14:58:00 -0300

    Criação da Calculadora de Gorjeta

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-function App() {
+const Aplicativo = styled.div`
+  text-align:center;
+`;
+const Form = styled.div`
+  background-color:#F9F9F9;
+  padding:20px;
+  margin-bottom:30px;
+`;
+const Titulo = styled.h1`
+  margin:0;
+  text-transform:uppercase;
+`;
+const Input = styled.input`
+  margin:30px 10px;
+  width:300px;
+  padding:10px 15px;
+  border:1 solid #CCC;
+  background-color:#F9F9F9;
+  border-radius:8px;
+`;
+
+function App(){
+
+  const [conta, setConta]       = useState(0);
+  const [gorjeta, setGorjeta]   = useState(10);
+
+  let valorConta    = parseFloat(conta).toFixed(2);
+  let valorGorjeta  = parseFloat((conta*gorjeta)/100).toFixed(2);
+  let total         = (parseFloat(conta) + parseFloat(valorGorjeta)).toFixed(2);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Aplicativo>
+      <Form>
+        <Titulo>Calculadora de Gorjeta!</Titulo>
+        <label>Valor da Conta?</label>
+        <Input type="number" value={conta} onChange={(e)=>{setConta(e.target.value)}}></Input>
+        <label>Porcentagem da Gorjeta?</label>
+        <Input type="number" value={gorjeta} onChange={(e)=>{setGorjeta(e.target.value)}}></Input>
+      </Form>
+      {conta != 0 &&
+        <>
+          <p>
+            Sub-Total: <b>R$ {valorConta}</b> - 
+            Gorjeta ({gorjeta}%): <b>R$ {valorGorjeta}</b>
+          </p>
+          <p><b>Total: R$ {total}</b></p>
+        </>
+      }
+    </Aplicativo>
   );
 }
 
